7e9f3e74fd1c32821460eb337c3f5f8b788d3573
Eddie Lebow <elebow@users.noreply.github.com>
2022-03-11 23:17:37 -0500

    Move Command to CrystalWindows::Command

--- src/crystal_windows/command.cr
+++ src/crystal_windows/command.cr
@@ -1,29 +1,32 @@
-class Command
-  getter string
+module CrystalWindows
+  class Command
+    getter string
 
-  def self.run(string)
-    new(string).run
-  end
+    def self.run(string)
+      new(string).run
+    end
 
-  def initialize(string : String?)
-    @string = string.as(String)
-  end
+    def initialize(string : String?)
+      @string = string.as(String)
+    end
 
-  def run
-    if string.nil? || string.matches?(/\A\s*\Z/)
-      CrystalWindows.debug "got nil command from socket"
-    elsif string == "list-windows"
-      CrystalWindows::X.all_windows.each { |win| puts win }
-    elsif string.starts_with?("raise-window")
-      pattern = string.lchop("raise-window").strip
-      win = CrystalWindows::X.find_window(pattern).first
-      CrystalWindows::X.raise_window(win[:id])
-    elsif string.starts_with?("hide-current-window")
-      CrystalWindows::X.hide_current_window
-    elsif string == "exit"
-      CrystalWindows.channel_send(:exit)
-    else
-      puts "unknown command: #{string}"
+    def run
+      if string.nil? || string.matches?(/\A\s*\Z/)
+        CrystalWindows.debug "got nil command from socket"
+      elsif string == "list-windows"
+        X.all_windows.each { |win| puts win }
+      elsif string.starts_with?("raise-window")
+        X.find_and_raise(string.lchop("raise-window").strip)
+      elsif string.starts_with?("cycle-category")
+        category_name = string.lchop("cycle-category").strip
+        CONFIG.categories.cycle(category_name)
+      elsif string.starts_with?("hide-current-window")
+        X.hide_current_window
+      elsif string == "exit"
+        CrystalWindows::CHANNEL.send(:exit)
+      else
+        puts "unknown command: #{string}"
+      end
     end
   end
 end
